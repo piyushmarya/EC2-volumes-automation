bd3a4d77b3dfa3e54d659134500d37a2f9dc8d10
piyushmarya <32293043+piyushmarya@users.noreply.github.com>
2020-01-08 15:26:04 +0530

    Update aws_shell.py

--- aws_shell.py
+++ aws_shell.py
@@ -1,21 +1,34 @@
-import cmd
 from cloud.aws_operations import AwsOperations
-from localdb.credential_handler import CredentialHandler
+
+cloud_obj = AwsOperations()
+
+USER_CHOICE = '''\nEnter one of the following
+- 'create' to create a new volume
+- 'delete' to delete
+- 'describe all' to display data all volumes in the specified region
+- 'attach' to attach a volume to a instance
+- 'detach' to detach a volume from a instance
+- list new' to print newly created volumes
+- 'q' to exit
+Enter your choice: '''
+
+user_choices = {
+    'create': cloud_obj.create_volume,
+    'delete': cloud_obj.delete_volume,
+    'detach': cloud_obj.detach_volume,
+    'attach': cloud_obj.attach_volume,
+    'describe all': cloud_obj.list_all_volumes,
+    'list new': cloud_obj.print_new_volumes
+}
 
 
-class shell_aws(cmd.Cmd):
+def menu():
+    user_input = input(USER_CHOICE)
+    while user_input != 'q':
+        if user_input in ('create', 'delete', 'detach', 'attach', 'describe all', 'list new'):
+            user_choices[user_input]()
+        else:
+            print('Please choose a valid command.')
+        user_input = input(USER_CHOICE)
 
-    def do_attach_volume(self, id):
-        print("Hello")
-
-    def do_detach_volume(self,id):
-        print("detach_volume")
-    
-    def do_list_instances(self):
-        print("list_instances")
-
-
-if __name__ == '__main__':
-    shell_aws().cmdloop()
-
-    
+menu()
